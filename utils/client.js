2091cf54019a43e034f646e420269b4c8a53bb16
JorgeFragaCal <jorge.fraga1997@gmail.com>
2022-05-23 14:12:09 +0200

    fix firebase

--- utils/client.js
+++ utils/client.js
@@ -1,6 +1,13 @@
 import { collection, getDocs, getDoc, doc } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
+import { initializeApp, getApps, getApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD61g2BrdWW5sIGZ9ZqADDyM5A5YFAVUa4",
+  authDomain: "gtrain-a06c6.firebaseapp.com",
+  projectId: "gtrain-a06c6",
+};
+getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore();
 
 const dbRef = collection(db, "usuarios");
